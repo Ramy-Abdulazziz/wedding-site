9309843dcca199671ccb2772d5a18c1b9967d170
Ramy <ramy@Kurama.localdomain>
2025-09-04 12:36:30 -0400

    fix: fixed issue with skeleton on light mode

--- components/completeProfileContent.jsx
+++ components/completeProfileContent.jsx
@@ -39,7 +39,7 @@ const CompleteProfileContent = () => {
         return (
             <Skeleton
                 className={cn(
-                    "mx-auto h-[500px] mt-35 max-w-[85%] lg:max-w-[75%] xl:max-w-[50%] 2xl:max-w-[50%] pb-[5vh] rounded-xl"
+                    "bg-gray-400/35 dark:bg-muted mx-auto h-[500px] mt-35 max-w-[85%] lg:max-w-[75%] xl:max-w-[50%] 2xl:max-w-[50%] pb-[5vh] rounded-xl"
                 )}
             />
         );
