e4632c4467e658bcf0e70789c91622ac8bb8bb01
Ramy <ramy@Kurama.localdomain>
2025-09-17 12:18:27 -0400

    fix: fixed issues with admin panel routes

--- utils/supabase/middleware.js
+++ utils/supabase/middleware.js
@@ -58,12 +58,12 @@ export async function updateSession(request) {
 
     const { data: guest, error: guestError } = await supabase
         .from("guests")
-        .select("id, email, phone")
+        .select("id, email, phone, groups(name)")
         .eq("id", user.id)
         .single();
 
     const verifiedUser = user && guest && !guestError && !userError;
-
+    const isAdmin = guest.groups.name === "Admin";
     if (!verifiedUser) {
         if (pathname !== authConfig.unAuthedHomeRoute) {
             const url = request.nextUrl.clone();
