155869b7e343c32f0e5b2690f727e97f8ee7a9ec
Ramy <ramy@Kurama.localdomain>
2025-09-18 04:23:48 -0400

    fix:added static import for performance

--- components/FloatingInvite.jsx
+++ components/FloatingInvite.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { ChevronsDownIcon } from "lucide-react";
 import { attentionDownItem } from "@/lib/variants";
 import { Button } from "./ui/button";
-
+import weddingInvite from "@/public/weddingInvite.jpg";
 
 const FloatingInvite = ({ ref }) => {
     const { scrollY } = useScroll();
@@ -26,10 +26,9 @@ const FloatingInvite = ({ ref }) => {
         >
             <div className={cn("relative w-full h-auto")}>
                 <Image
-                    src="/weddingInvite.jpg"
-                    width={600}
-                    height={800}
+                    src={weddingInvite}
                     priority={true}
+                    placeholder="blur"
                     sizes="(max-width: 533px) 85vw, 400px"
                     quality={100}
                     alt="Picture of wedding invitation"
