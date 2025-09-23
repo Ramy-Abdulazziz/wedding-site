9666fcb3e3ae45ca6c0c02d55f336bbe9a82ee02
Ramy <ramy@Kurama.localdomain>
2025-09-23 15:52:36 -0400

    fix/updated-faq-text-style

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -4,19 +4,26 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion";
+import { cn } from "@/lib/utils";
 
 const Faq = () => {
     return (
         <Accordion
             type="single"
             collapsible
-            className="w-full mr-6 md:mr-7 xl:mr-26"
+            className={cn("w-full mr-6 md:mr-7 xl:mr-26")}
         >
             <AccordionItem value="item-1">
-                <AccordionTrigger>
+                <AccordionTrigger
+                    className={cn("text-md lg:text-lg xl:text-lg 2xl:text-xl")}
+                >
                     Will My Dietary Restrictions Be Accommodated?
                 </AccordionTrigger>
-                <AccordionContent className="flex flex-col gap-4 text-balance">
+                <AccordionContent
+                    className={cn(
+                        "text-lg flex flex-col gap-4 text-pretty text-md lg:text-lg xl:text-lg 2xl:text-xl"
+                    )}
+                >
                     <p>
                         All food at the reception will be fully Halal, with
                         vegan and vegetarian options available as well.
@@ -25,11 +32,20 @@ const Faq = () => {
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-2">
-                <AccordionTrigger>Are Children Invited?</AccordionTrigger>
-                <AccordionContent className="flex flex-col gap-4 text-balance">
+                <AccordionTrigger
+                    className={cn("text-md lg:text-lg xl:text-lg 2xl:text-xl")}
+                >
+                    Are Children Invited?
+                </AccordionTrigger>
+                <AccordionContent
+                    className={cn(
+                        "flex flex-col gap-4 text-pretty text-md lg:text-lg xl:text-lg 2xl:text-xl"
+                    )}
+                >
                     <p>
                         We adore your little ones, but we kindly ask that our
-                        celebration remain adults-only. Therefore, no children are allowed.
+                        celebration remain adults-only. As such, no children
+                        are allowed.
                     </p>
                     <p>
                         We truly appreciate your understanding and can't wait to
@@ -38,10 +54,16 @@ const Faq = () => {
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
-                <AccordionTrigger>
+                <AccordionTrigger
+                    className={cn("text-md lg:text-lg xl:text-lg 2xl:text-xl")}
+                >
                     I missed the RSVP deadline - can I still respond?
                 </AccordionTrigger>
-                <AccordionContent className="flex flex-col gap-4 text-balance">
+                <AccordionContent
+                    className={cn(
+                        "flex flex-col gap-4 text-pretty text-md lg:text-lg xl:text-lg 2xl:text-xl"
+                    )}
+                >
                     <p>
                         Unfortunately we aren't able to accept RSVPs after the
                         deadline as we've already finalized details with our
@@ -54,10 +76,16 @@ const Faq = () => {
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-4">
-                <AccordionTrigger>
+                <AccordionTrigger
+                    className={cn("text-md lg:text-lg xl:text-lg 2xl:text-xl")}
+                >
                     Can I bring additional guests?
                 </AccordionTrigger>
-                <AccordionContent className="flex flex-col gap-4 text-balance">
+                <AccordionContent
+                    className={cn(
+                        "flex flex-col gap-4 text-pretty text-md lg:text-lg xl:text-lg 2xl:text-xl"
+                    )}
+                >
                     <p>
                         We are only able to accommodate the guests listed on
                         your invitation.
@@ -66,19 +94,33 @@ const Faq = () => {
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-5">
-                <AccordionTrigger>What is the dress code?</AccordionTrigger>
-                <AccordionContent className="flex flex-col gap-4 text-balance">
+                <AccordionTrigger
+                    className={cn("text-md lg:text-lg xl:text-lg 2xl:text-xl")}
+                >
+                    What is the dress code?
+                </AccordionTrigger>
+                <AccordionContent
+                    className={cn(
+                        "flex flex-col gap-4 text-pretty text-md lg:text-lg xl:text-lg 2xl:text-xl"
+                    )}
+                >
                     <p>
                         We kindly ask all guests to observe a formal dress code.
                     </p>
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-6">
-                <AccordionTrigger>
+                <AccordionTrigger
+                    className={cn("text-md lg:text-lg xl:text-lg 2xl:text-xl")}
+                >
                     What if i have questions, need help, or have issues with the
                     RSVP process?
                 </AccordionTrigger>
-                <AccordionContent className="flex flex-col gap-4 text-balance">
+                <AccordionContent
+                    className={cn(
+                        "flex flex-col gap-4 text-pretty text-md lg:text-lg xl:text-lg 2xl:text-xl"
+                    )}
+                >
                     <p>
                         If you have any issues, questions, or concerns please
                         reach out to Ramy and Shazia via text at 631-869-7432.
